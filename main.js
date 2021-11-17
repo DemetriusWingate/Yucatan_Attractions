00808a42a1dac83b2477245702824f539a6b59e2
Demetrius Wingate <dwingate1991@gmail.com>
2021-11-17 17:13:23 -0500

    Added random Generator

---
+++ main.js
@@ -0,0 +1,36 @@
+    /* This function switches the nav menus */
+    function myNav () {
+        var x=document.getElementById("navLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+  
+  
+  /*this function validates the form*/
+  
+  function myFunction () {
+      var x=document.getElementById('name').value;
+      if (x == "") {
+      alert ("You didn't tell us your name!");
+          return false;
+       } else {
+      alert ("Thanks, " + x + " for signing up for our newsletter!");
+      }
+      }
+  
+      /* Trip Generator*/
+  function randomAttractionGenerator() {
+    const attractions = [
+      "Pink Lakes",
+      "Cenotes",
+      "Yellow City",
+      "Merida",
+      "Mayan Ruins"
+    ];
+  
+    const random = attractions[Math.random() * attractions.length | 0]
+    alert("This looks like the perfect match for you: " + random, attractions[random]);
+  }
