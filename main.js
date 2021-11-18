d863161d99b4893b709831407dd58398c63a9972
Demetrius Wingate <dwingate1991@gmail.com>
2021-11-18 00:04:23 -0500

    Added Countdown Feature

--- main.js
+++ main.js
@@ -7,6 +7,52 @@
           x.style.display = "block";
         }
       }
+
+      /*Countdown Timer*/
+
+      (function () {
+        const second = 1000,
+              minute = second * 60,
+              hour = minute * 60,
+              day = hour * 24;
+      
+        //Auto update yearly so time never runs out
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy + 1,
+            dayMonth = "07/10/",
+            summer = dayMonth + yyyy;
+        
+        today = mm + "/" + dd + "/" + yyyy;
+        if (today > summer) {
+          summer = dayMonth + nextYear;
+        }
+        
+        //countdown 
+        
+        const countDown = new Date(summer).getTime(),
+            x = setInterval(function() {    
+      
+              const now = new Date().getTime(),
+                    distance = countDown - now;
+      
+              document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+      
+              //do something later when date is reached
+              if (distance < 0) {
+                document.getElementById("headline").innerText = "New Tours Are Here!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+              }
+              //seconds
+            }, 0)
+        }());
   
 
       /* Random Attraction Generator*/
